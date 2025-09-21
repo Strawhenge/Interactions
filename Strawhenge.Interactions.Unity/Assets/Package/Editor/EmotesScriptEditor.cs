03d47e4abd0bea087c545c54179267485cf23a11
Strawhenge <strawhengestudios@gmail.com>
2025-09-21 23:50:06 +0100

    initial editor project

---
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/EmotesScriptEditor.cs
@@ -0,0 +1,10 @@
+using Strawhenge.Interactions.Unity.Emotes;
+using UnityEditor;
+
+namespace Strawhenge.Interactions.Unity.Editor
+{
+    [CustomEditor(typeof(EmotesScript))]
+    public class EmotesScriptEditor : UnityEditor.Editor
+    {
+    }
+}
