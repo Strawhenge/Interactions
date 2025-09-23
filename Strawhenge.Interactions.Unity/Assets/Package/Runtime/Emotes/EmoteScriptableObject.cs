4646cc316c031468640837e8e70be7c10f39ea35
Strawhenge <strawhengestudios@gmail.com>
2025-09-24 00:46:45 +0100

    added item to emote data

--- Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmoteScriptableObject.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmoteScriptableObject.cs
@@ -1,4 +1,5 @@
 using FunctionalUtilities;
+using Strawhenge.Inventory.Unity.Items.ItemData;
 using UnityEngine;
 
 namespace Strawhenge.Interactions.Unity.Emotes
@@ -7,7 +8,10 @@ namespace Strawhenge.Interactions.Unity.Emotes
     public class EmoteScriptableObject : ScriptableObject
     {
         [SerializeField] AnimationClip _animation;
+        [SerializeField] ItemScriptableObject _item;
 
         public Maybe<AnimationClip> Animation => Maybe.NotNull(_animation);
+
+        public Maybe<ItemScriptableObject> Item => Maybe.NotNull(_item);
     }
 }
