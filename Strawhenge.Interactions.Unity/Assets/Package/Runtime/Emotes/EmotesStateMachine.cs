3773fe117f98a8a3340054c7ac1ebd365e98a0d0
Strawhenge <strawhengestudios@gmail.com>
2025-09-24 00:14:29 +0100

    implemented animator statemachine for emotes

--- Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmotesStateMachine.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmotesStateMachine.cs
@@ -1,8 +1,17 @@
+using Strawhenge.Common.Unity.AnimatorBehaviours;
+using System;
 using UnityEngine;
 
 namespace Strawhenge.Interactions.Unity.Emotes
 {
-    public class EmotesStateMachine : StateMachineBehaviour
+    public class EmotesStateMachine : StateMachineBehaviour, IHasDestroyedEvent
     {
+        public event Action Destroyed;
+
+        public override void OnStateMachineExit(Animator animator, int stateMachinePathHash) => OnEmoteEnded();
+
+        internal Action OnEmoteEnded = () => { };
+
+        void OnDestroy() => Destroyed?.Invoke();
     }
 }
