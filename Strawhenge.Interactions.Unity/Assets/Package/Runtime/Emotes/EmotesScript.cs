cd6e6c8e6b16b187690dfaac925386f4676c6ec5
Strawhenge <strawhengestudios@gmail.com>
2025-09-23 23:30:50 +0100

    added animator to emotes script

--- Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmotesScript.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmotesScript.cs
@@ -1,9 +1,19 @@
+using Strawhenge.Common.Unity.Helpers;
+using System;
 using UnityEngine;
 
 namespace Strawhenge.Interactions.Unity.Emotes
 {
     public class EmotesScript : MonoBehaviour
     {
+        [SerializeField] Animator _animator;
+
+        void Awake()
+        {
+            // TODO ComponentRefHelper.EnsureRootHierarchyComponent(ref _animator, nameof(_animator), this);
+            ComponentRefHelper.EnsureHierarchyComponent(ref _animator, nameof(_animator), this);
+        }
+
         public void Perform(EmoteScriptableObject emote)
         {
             Debug.Log($"Performing emote '{emote.name}'.", emote);
