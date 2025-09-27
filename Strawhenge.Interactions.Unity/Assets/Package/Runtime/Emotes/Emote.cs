fd0d5b705cb1ebb730aeba67a6a045b288f43358
Strawhenge <strawhengestudios@gmail.com>
2025-09-27 22:39:25 +0100

    implemented emote

--- Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/Emote.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/Emote.cs
@@ -10,6 +10,10 @@ namespace Strawhenge.Interactions.Unity.Emotes
         readonly Maybe<Inventory.Inventory> _inventory;
         readonly EmoteScriptableObject _emote;
 
+        InventoryItem _item;
+        Action _onStopped;
+        bool _animationStarted;
+
         public Emote(
             EmoteAnimationHandler animationHandler,
             Maybe<Inventory.Inventory> inventory,
@@ -22,10 +26,70 @@ namespace Strawhenge.Interactions.Unity.Emotes
 
         protected override void OnStart()
         {
+            if (HasItem(out _item))
+            {
+                _item.HoldRightHand(StartAnimation);
+                return;
+            }
+
+            StartAnimation();
+        }
+
+        void StartAnimation()
+        {
+            _animationStarted = true;
+            _animationHandler.AnimationEnded += OnAnimationEndedBySelf;
+            _animationHandler.Perform(_emote.Animation);
+        }
+
+        void OnAnimationEndedBySelf()
+        {
+            _animationHandler.AnimationEnded -= OnAnimationEndedBySelf;
+
+            if (_item != null)
+            {
+                _item.ClearFromHands(InvokeStopped);
+                return;
+            }
+
+            InvokeStopped();
         }
 
         protected override void OnStop(Action onStopped)
         {
+            if (!_animationStarted)
+            {
+                if (_item != null)
+                    _item.ClearFromHands(onStopped);
+                else
+                    onStopped();
+
+                return;
+            }
+
+            _onStopped = onStopped;
+            _animationHandler.AnimationEnded -= OnAnimationEndedBySelf;
+            _animationHandler.AnimationEnded += OnAnimationEndedByRequest;
+            _animationHandler.End();
+        }
+
+        void OnAnimationEndedByRequest()
+        {
+            _animationHandler.AnimationEnded -= OnAnimationEndedByRequest;
+
+            if (_item != null)
+                _item.ClearFromHands(_onStopped);
+            else
+                _onStopped();
+        }
+
+        bool HasItem(out InventoryItem item)
+        {
+            return _emote.Item
+                .Map(i => _inventory
+                    .Map(inventory => inventory.GetItemOrCreateTemporary(i.ToItem())))
+                .Flatten()
+                .HasSome(out item);
         }
     }
 }
