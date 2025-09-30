7cbb4fc117b34bd76f27a4c36735b9bf139e0e51
Strawhenge <strawhengestudios@gmail.com>
2025-09-30 20:56:20 +0100

    implemented parameter setup

--- Strawhenge.Interactions.Unity/Assets/Package/Editor/EmotesAnimatorSetup.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/EmotesAnimatorSetup.cs
@@ -1,4 +1,5 @@
 using UnityEditor.Animations;
+using UnityEngine;
 
 namespace Strawhenge.Interactions.Unity.Editor
 {
@@ -6,6 +7,32 @@ namespace Strawhenge.Interactions.Unity.Editor
     {
         public static void Setup(AnimatorController animatorController)
         {
+            AddParameters(animatorController);
+        }
+
+        static void AddParameters(AnimatorController animatorController)
+        {
+            // TODO Move to single location
+            const string BeginEmote = "Begin Emote";
+            const string EndEmote = "End Emote";
+
+            bool hasBeginParameter = false;
+            bool hasEndParameter = false;
+
+            foreach (var parameter in animatorController.parameters)
+            {
+                if (parameter.name == BeginEmote)
+                    hasBeginParameter = true;
+
+                if (parameter.name == EndEmote)
+                    hasEndParameter = true;
+            }
+
+            if (!hasBeginParameter)
+                animatorController.AddParameter(BeginEmote, AnimatorControllerParameterType.Trigger);
+
+            if (!hasEndParameter)
+                animatorController.AddParameter(EndEmote, AnimatorControllerParameterType.Trigger);
         }
     }
 }
