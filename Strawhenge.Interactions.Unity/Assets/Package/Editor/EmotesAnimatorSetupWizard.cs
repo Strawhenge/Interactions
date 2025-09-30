ef084a7af98ade8793f60405457d3e978730c31a
Strawhenge <strawhengestudios@gmail.com>
2025-09-30 20:45:18 +0100

    initial wizard

---
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/EmotesAnimatorSetupWizard.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Strawhenge.Interactions.Unity.Editor
+{
+    public class EmotesAnimatorSetupWizard : ScriptableWizard
+    {
+        const string Name = "Emotes Animator Setup";
+
+        [MenuItem("Strawhenge/" + Name)]
+        public static void ShowEditorWindow()
+        {
+            DisplayWizard<EmotesAnimatorSetupWizard>(Name, "Set Up");
+        }
+
+        [SerializeField] AnimatorController _animatorController;
+
+        void OnWizardCreate()
+        {
+            Debug.Log("Emotes Animator Setup");
+        }
+    }
+}
