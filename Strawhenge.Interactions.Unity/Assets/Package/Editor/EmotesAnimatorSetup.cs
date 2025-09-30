a8e26dd6e76d8cc400eeedd96ad7a2888ded6274
Strawhenge <strawhengestudios@gmail.com>
2025-09-30 21:28:36 +0100

    implemented emote substate and transitions setup

--- Strawhenge.Interactions.Unity/Assets/Package/Editor/EmotesAnimatorSetup.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/EmotesAnimatorSetup.cs
@@ -1,3 +1,4 @@
+using Strawhenge.Interactions.Unity.Emotes;
 using UnityEditor.Animations;
 using UnityEngine;
 
@@ -5,17 +6,18 @@ namespace Strawhenge.Interactions.Unity.Editor
 {
     static class EmotesAnimatorSetup
     {
+        // TODO Move to single location
+        const string BeginEmote = "Begin Emote";
+        const string EndEmote = "End Emote";
+
         public static void Setup(AnimatorController animatorController)
         {
             AddParameters(animatorController);
+            AddSubStateMachine(animatorController);
         }
 
         static void AddParameters(AnimatorController animatorController)
         {
-            // TODO Move to single location
-            const string BeginEmote = "Begin Emote";
-            const string EndEmote = "End Emote";
-
             bool hasBeginParameter = false;
             bool hasEndParameter = false;
 
@@ -33,6 +35,31 @@ namespace Strawhenge.Interactions.Unity.Editor
 
             if (!hasEndParameter)
                 animatorController.AddParameter(EndEmote, AnimatorControllerParameterType.Trigger);
+        }
+
+        static void AddSubStateMachine(AnimatorController animatorController)
+        {
+            AnimatorStateMachine rootStateMachine = animatorController.layers[0].stateMachine;
+
+            var emotesStateMachine = rootStateMachine.AddStateMachine("Emotes");
+            emotesStateMachine.AddStateMachineBehaviour<EmotesStateMachine>();
+
+            var emoteState = emotesStateMachine.AddState("Emote");
+            // TODO Set animation clip
+
+            var anyStateTransition = rootStateMachine.AddAnyStateTransition(emoteState);
+            anyStateTransition.AddCondition(AnimatorConditionMode.If, 0, BeginEmote);
+            anyStateTransition.hasExitTime = false;
+
+            var endEmoteTransition = emoteState.AddExitTransition();
+            endEmoteTransition.AddCondition(AnimatorConditionMode.If, 0, EndEmote);
+            endEmoteTransition.hasExitTime = false;
+
+            var animationEndedTransition = emoteState.AddExitTransition();
+            animationEndedTransition.hasExitTime = true;
+
+            if (rootStateMachine.defaultState != null && rootStateMachine.defaultState != emoteState)
+                rootStateMachine.AddStateMachineTransition(emotesStateMachine, rootStateMachine.defaultState);
         }
     }
 }
