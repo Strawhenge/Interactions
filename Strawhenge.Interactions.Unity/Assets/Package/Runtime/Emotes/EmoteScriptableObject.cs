349a14a4c8a308ce0dc831f7996e2b9c3d645578
Strawhenge <strawhengestudios@gmail.com>
2025-09-30 19:50:02 +0100

    added animator parameter scriptable object

--- Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmoteScriptableObject.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Runtime/Emotes/EmoteScriptableObject.cs
@@ -1,5 +1,8 @@
 using FunctionalUtilities;
+using Strawhenge.Common;
+using Strawhenge.Interactions.Unity.Package.Runtime;
 using Strawhenge.Inventory.Unity.Items.ItemData;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Strawhenge.Interactions.Unity.Emotes
@@ -9,9 +12,13 @@ namespace Strawhenge.Interactions.Unity.Emotes
     {
         [SerializeField] AnimationClip _animation;
         [SerializeField] ItemScriptableObject _item;
+        [SerializeField] AnimatorBoolParameterScriptableObject[] _animatorBoolParameters;
 
         public Maybe<AnimationClip> Animation => Maybe.NotNull(_animation);
 
         public Maybe<ItemScriptableObject> Item => Maybe.NotNull(_item);
+
+        public IEnumerable<AnimatorBoolParameterScriptableObject> AnimatorBoolParameters =>
+            _animatorBoolParameters.ExcludeNull();
     }
 }
