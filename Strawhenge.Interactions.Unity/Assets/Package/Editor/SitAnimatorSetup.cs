a107440d7ac1b3088b9dbace0693353ea50efe4a
Strawhenge <strawhengestudios@gmail.com>
2025-10-22 02:50:19 +0100

    initial sit animator setup wizard

---
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/SitAnimatorSetup.cs
@@ -0,0 +1,13 @@
+using UnityEditor.Animations;
+
+namespace Strawhenge.Interactions.Unity.Editor
+{
+    static class SitAnimatorSetup
+    {
+        public static void Setup(
+            AnimatorController animatorController,
+            int layerIndex)
+        {
+        }
+    }
+}
---
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/SitAnimatorSetupWizard.cs
@@ -0,0 +1,63 @@
+using Strawhenge.Common;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Strawhenge.Interactions.Unity.Editor
+{
+    public class SitAnimatorSetupWizard : ScriptableWizard
+    {
+        const string Name = "Sit Animator Setup";
+
+        [MenuItem("Strawhenge/" + Name)]
+        public static void ShowEditorWindow()
+        {
+            DisplayWizard<SitAnimatorSetupWizard>(Name, "Set Up");
+        }
+
+        [SerializeField] AnimatorController _animatorController;
+
+        AnimatorController _selectedController;
+        string[] _layers;
+        int _selectedLayerIndex;
+
+        protected override bool DrawWizardGUI()
+        {
+            if (_selectedController == null)
+                return base.DrawWizardGUI();
+
+            var result = base.DrawWizardGUI();
+
+            _selectedLayerIndex = EditorGUILayout.Popup("Layer", _selectedLayerIndex, _layers);
+
+            return result;
+        }
+
+        void OnWizardUpdate()
+        {
+            if (_selectedController == _animatorController) return;
+            _selectedController = _animatorController;
+
+            if (_selectedController == null) return;
+
+            UpdateLayers();
+        }
+
+        void OnWizardCreate()
+        {
+            if (_selectedController == null)
+            {
+                Debug.LogWarning("No controller selected.");
+                return;
+            }
+
+            SitAnimatorSetup.Setup(_animatorController, _selectedLayerIndex);
+        }
+
+        void UpdateLayers()
+        {
+            _layers = _selectedController.layers.ToArray(layer => layer.name);
+        }
+    }
+}
