78782d5e400f15c5426458fc5ee4af1ccd046007
Strawhenge <strawhengestudios@gmail.com>
2025-10-23 01:46:13 +0100

    added sleep script

---
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/SleepScriptEditor.cs
@@ -0,0 +1,42 @@
+using Strawhenge.Interactions.Unity.Sleep;
+using UnityEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Strawhenge.Interactions.Unity.Editor
+{
+    [CustomEditor(typeof(SleepScript))]
+    public class SleepScriptEditor : UnityEditor.Editor
+    {
+        SleepScript _sleep;
+        SleepAnimationsScriptableObject _animations;
+
+        void OnEnable()
+        {
+            _sleep ??= target as SleepScript;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.Separator();
+            EditorGUI.BeginDisabledGroup(!Application.isPlaying);
+
+            _animations = EditorGUILayout.ObjectField(
+                "Animations",
+                _animations,
+                typeof(SleepAnimationsScriptableObject),
+                allowSceneObjects: false) as SleepAnimationsScriptableObject;
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button(nameof(SleepController.GoToSleep)))
+                _sleep.SleepController.GoToSleep(_animations);
+
+            if (GUILayout.Button(nameof(SleepController.WakeUp)))
+                _sleep.SleepController.WakeUp();
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
