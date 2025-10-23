3ba38e0dbc873df5f7a42fe79c1b1e21d9b87fb8
Strawhenge <strawhengestudios@gmail.com>
2025-10-23 01:38:16 +0100

    added sleep script

---
+++ Strawhenge.Interactions.Unity/Assets/Package/Runtime/Sleep/SleepScript.cs
@@ -0,0 +1,27 @@
+using Strawhenge.Common.Unity.Helpers;
+using Strawhenge.Common.Unity.Serialization;
+using UnityEngine;
+
+namespace Strawhenge.Interactions.Unity.Sleep
+{
+    public class SleepScript : MonoBehaviour
+    {
+        [SerializeField] Animator _animator;
+
+        [SerializeField] SerializedSource<
+            ISleepAnimations,
+            SerializedSleepAnimations,
+            SleepAnimationsScriptableObject> _defaultAnimations;
+
+        SleepController _sleepController;
+
+        public SleepController SleepController => _sleepController ??= CreateSleepController();
+
+        SleepController CreateSleepController()
+        {
+            ComponentRefHelper.EnsureRootHierarchyComponent(ref _animator, nameof(_animator), this);
+
+            return new SleepController(_animator, _defaultAnimations.GetValue());
+        }
+    }
+}
