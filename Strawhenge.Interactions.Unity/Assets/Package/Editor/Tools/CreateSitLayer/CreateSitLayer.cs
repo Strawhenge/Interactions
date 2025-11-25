df1c067197bd1858690057f53a987b4f2ea914cc
Strawhenge <strawhengestudios@gmail.com>
2025-11-25 20:49:59 +0000

    added sit type id to aniimator parameters

--- Strawhenge.Interactions.Unity/Assets/Package/Editor/Tools/CreateSitLayer/CreateSitLayer.cs
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/Tools/CreateSitLayer/CreateSitLayer.cs
@@ -25,12 +25,16 @@ namespace Strawhenge.Interactions.Unity.Editor
         static void AddParameters(AnimatorController animatorController)
         {
             bool hasSitParameter = false;
+            bool hasSitTypeIdParameter = false;
             bool hasStandParameter = false;
 
             foreach (var parameter in animatorController.parameters)
             {
                 if (parameter.name == AnimatorParameters.Sit.Name)
                     hasSitParameter = true;
+
+                if (parameter.name == AnimatorParameters.SitTypeId.Name)
+                    hasSitTypeIdParameter = true;
 
                 if (parameter.name == AnimatorParameters.Stand.Name)
                     hasStandParameter = true;
@@ -39,6 +43,10 @@ namespace Strawhenge.Interactions.Unity.Editor
             if (!hasSitParameter)
                 animatorController
                     .AddParameter(AnimatorParameters.Sit.Name, AnimatorControllerParameterType.Trigger);
+
+            if (!hasSitTypeIdParameter)
+                animatorController
+                    .AddParameter(AnimatorParameters.SitTypeId.Name, AnimatorControllerParameterType.Int);
 
             if (!hasStandParameter)
                 animatorController
