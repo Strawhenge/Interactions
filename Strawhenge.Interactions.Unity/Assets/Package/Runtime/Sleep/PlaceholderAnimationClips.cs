08e57f7a0735b8e9344919df124f07def7d84ff2
Strawhenge <strawhengestudios@gmail.com>
2025-11-25 22:48:52 +0000

    removed obsolete code

--- Strawhenge.Interactions.Unity/Assets/Package/Runtime/Sleep/PlaceholderAnimationClips.cs
+++
@@ -1,9 +0,0 @@
-namespace Strawhenge.Interactions.Unity.Sleep
-{
-    static class PlaceholderAnimationClips
-    {
-        public const string LayDown = "Placeholder Lay Down";
-        public const string Sleeping = "Placeholder Sleeping";
-        public const string GetUp = "Placeholder Get Up";
-    }
-}
