c628a16d044a4892d128355f3d9d16ab150d0d36
Strawhenge <strawhengestudios@gmail.com>
2025-11-25 20:32:30 +0000

    initial sit type wizard

---
+++ Strawhenge.Interactions.Unity/Assets/Package/Editor/Tools/CreateSitType/CreateSitType.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Strawhenge.Interactions.Unity.Editor
+{
+    static class CreateSitType
+    {
+        public static void Create(
+            AnimatorController animatorController,
+            string name,
+            AnimationClip sitAnimation,
+            AnimationClip sittingAnimation,
+            AnimationClip standAnimation)
+        {
+        }
+    }
+}
